ae646a197c5871a3255b93fa6595f0692cf9438d
alejandroDpAL <alejandropasaje799@gmail.com>
2025-12-25 22:30:36 -0500

    cambios creación .env

--- pollosmovil/src/Hook/Api/apiConnection.js
+++ pollosmovil/src/Hook/Api/apiConnection.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-    baseURL: "http://192.168.100.11:3000",
+    baseURL: "http://192.168.100.100:3000",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
